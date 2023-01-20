c1b6504d9600d1ce4486b29c885b004a60b8bc4b
Josue Armando Uriarte Reyes <47550290+JosueUriarte@users.noreply.github.com>
2023-01-19 23:20:05 -0800

    Website now builds correctly

--- vite.config.js
+++ vite.config.js
@@ -2,34 +2,39 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 // Slightly modified from https://github.com/alexlafroscia/vite-plugin-handlebars
 import handlebars from "@glitchdotcom/vite-plugin-handlebars";
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
-export default defineConfig(async ({ command, mode }) => {
-  return {
-    plugins: [
-      handlebars({
-        partialDirectory: resolve(__dirname, "layout"),
-        settingsFile: 'settings.json',
-        helpers: {
-          hostasclass: value => new URL(value).hostname.replace(/\./g, "_")
-        },
-      //   reloadOnPartialChange: true
-      })
-    ]
-  //   build: {
-  //     cssCodeSplit: false,
-  //     outDir: "build"
-  //   },
-  //   optimizeDeps: {
-  //     exclude: ['./settings.json']
-  //   },
-  //   server: {
-  //     host:"0.0.0.0",
-  //     port:3000,
-  //     strictPort: true,
-  //     hmr: {
-  //       clientPort: 443
-  //     }
-  //   }
-  };
-});
+export default defineConfig(({ command, mode }) => {
+    return{
+      plugins: [
+        mkcert(),
+        handlebars({
+          partialDirectory: resolve(__dirname, './layout'),
+          settingsFile: 'settings.json',
+          helpers: {
+            hostasclass: value => new URL(value).hostname.replace(/\./g, "_")
+          },
+          reloadOnPartialChange: true
+        }),
+      ],
+      root: '.',
+      // server: {
+      //   https: true,
+      //   host: true,
+      //   port:3000,
+      //   strictPort: true,
+      //   hmr: {
+      //     clientPort: 443
+      //   }
+      // },
+      optimizeDeps:{
+        exclude: ['./settings.json']
+      },
+      build: {
+        cssCodeSplit: false,
+        outDir: "build"
+      },
+    }
+})
+
