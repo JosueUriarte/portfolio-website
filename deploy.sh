90bf302645570c1153ad2e5824b7a92da674f947
Josue Armando Uriarte Reyes <uriartejosue@gmail.com>
2023-01-22 16:49:16 -0800

    Cleaning deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,48 +1,3 @@
-# #!/usr/bin/env sh
-
-# # abort on errors
-# # set -e
-
-# # build
-# npm run build
-
-# # navigate into the build output directory
-# cd build
-
-# # place .nojekyll to bypass Jekyll processing
-# # echo > .nojekyll
-
-# # if you are deploying to a custom domain
-# # echo 'www.josueuriarte.dev' > CNAME
-
-# # git init
-# git checkout gh-pages
-# git status
-
-# # git --a "Deploying New Build"
-
-# # git checkout -B main
-# git add -A
-# git commit -m 'deploying new Vite Build'
-# git push portfolio-website gh-pages
-
-# # if you are deploying to https://<USERNAME>.github.io
-# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# # git push -f git@github.com:JosueUriarte/portfolio-website.git main:gh-pages
-# # git subtree add --prefix build https://github.com/JosueUriarte/portfolio-website/tree/gh-pages master --squash
-# # git subtree push-all --prefix build https://github.com/JosueUriarte/portfolio-website/tree/gh-pages master --squash
-# # git push -f git@github.com:JosueUriarte/portfolio-website.git main:gh-pages
-
-# # git checkout main
-# .. cd
-
-# # This link helped me deploy the website better
-# # https://dev.to/shashannkbawa/deploying-vite-app-to-github-pages-3ane
-
-# # git subtree push --prefix build origin gh-pages
-
 #!/usr/bin/env sh
 
 # abort on errors
@@ -63,10 +18,10 @@ echo 'www.josueuriarte.dev' > CNAME
 git init
 git checkout -B main
 git add -A
-git commit -m 'Deploying!!! Deployed on: `date`'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+current="`date +'%Y-%m-%d %H:%M:%S'`"
+msg="Deployed on: $current"
+git commit -m "Deploying!!! $msg"
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/JosueUriarte/portfolio-website.git main:gh-pages
